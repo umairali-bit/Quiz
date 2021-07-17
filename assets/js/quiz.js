bc120da4b40487bb8ab4351f1699f656ca4eeae7
Umair Ali <umairmamoor@gmail.com>
2021-07-17 09:25:54 -0400

    Welcome Page

---
+++ assets/js/quiz.js
@@ -0,0 +1,27 @@
+var questions = [
+    {
+      question: "Commonly used data types DO NOT include:",
+      choices: ["strings", "booleans", "alerts", "numbers"],
+      answer: "alerts",
+    },
+    {
+      question:
+        "The condition in an if / else statement is enclosed within ____.",
+      choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+      answer: "parentheses",
+    },
+  ];
+  
+  var introEl = document.querySelector("intro");
+  var introHeadingEl = document.querySelector("intro-heading");
+  var intropEl = document.querySelector("intro-p");
+  var buttonEl = document.querySelector("button");
+  var questionEl = document.querySelector("#question");
+  var optionListEl = document.querySelector("#option-list");
+  var questionResultEl = document.querySelector("#question-result");
+  var timerEl = document.querySelector("#timer");
+  
+  
+
+
+  
